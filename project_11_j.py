a2e01d0e6e35b6c54a88962ec2b9b5cd65b3d4e4
varaljai <71698297+varaljai@users.noreply.github.com>
2022-11-29 11:35:24 +0100

    Update project_11_j.py

--- project_11_j.py
+++ project_11_j.py
@@ -1,9 +1,15 @@
 import sqlite3
 with open("periodusos_rendszer.txt") as elem:
-    lista = [sor.strip().split("\t") for sor in elem]
-    
-print(lista)
+    lista = [sor.strip().split(",") for sor in elem]
+
 
 con = sqlite3.connect("adatok.db")
-cor = con.cursor()
+cur = con.cursor()
+
+cur.execute("CREATE TABLE periodusos_tabla(AtomicNumber INT, Element TEXT, Symbol TEXT, AtomicMass FLOAT)")
+
+cur.executemany("INSERT INTO periodusos_tabla VALUES(?,?,?,?)", lista)
+con.commit()
+
+print(lista)
 
